6892378742d6cfa52cf3276193f23d635545afd8
aminoru- <aminoru-@student.42sp.org.br>
2022-04-06 15:49:43 +0200

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 15:11:29 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/06 15:26:45 by aminoru-         ###   ########.fr       */
+/*   Created: 2022/04/06 15:24:26 by aminoru-          #+#    #+#             */
+/*   Updated: 2022/04/06 15:47:04 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-/*
-** -------------------
-** -- Libc Functions--
-** -------------------
-*/
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *ptr, int value, size_t num);
-void	ft_bzero(void *s, size_t n);
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, '\0', n);
+}
