5c8c12151eabd81119d67ad439b90a0a3559887c
aminoru- <aminoru-@student.42sp.org.br>
2022-04-27 22:13:08 +0200

    fix bug - ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 21:14:46 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/20 21:53:17 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/04/27 22:11:13 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
-	size_t	i;
-	size_t	s_len;
+	size_t	strlen_s;
 
-	substr = malloc((len + 1) * sizeof(char));
-	if (!s || !substr)
+	if (!s)
 		return (NULL);
-	i = 0;
-	s_len = ft_strlen(s);
-	while ((i < len) && (start + i) < s_len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = '\0';
+	strlen_s = ft_strlen((char *)s);
+	if (start > strlen_s)
+		return (ft_strdup(""));
+	if (strlen_s - start >= len)
+		substr = malloc((len + 1) * sizeof(char));
+	else
+		substr = malloc((strlen_s - start + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, (s + start), (len + 1));
 	return (substr);
 }
