9b855d55c04cc387619517497f75979480deadf4
aminoru- <aminoru-@student.42sp.org.br>
2022-04-27 15:59:17 +0200

    fix bug - ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 02:33:19 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/14 02:49:39 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/04/27 15:57:15 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] && s2[i] && i < n && s1[i] == s2[i])
+	while (s1[i] && s2[i] && i < n -1 && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
