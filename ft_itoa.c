019e41371e3d6503861e4ab0d0c2ff12fa3d9acc
aminoru- <aminoru-@student.42sp.org.br>
2022-04-27 23:05:37 +0200

    fiz bug - ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,55 +6,44 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 23:21:11 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/25 15:08:00 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/04/27 23:04:50 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_abs(int n)
+int	ft_sizeint(long int n)
 {
-	if (n > 0)
-		return (n);
-	return (-n);
+	if (n < 0)
+		return (1 + ft_sizeint(-n));
+	if ((n / 10) == 0)
+		return (1);
+	else
+		return (1 + ft_sizeint(n / 10));
 }
 
-size_t	ft_sizeint(int n)
+void	ft_int_to_char(long int n, char *num, long int i)
 {
-	size_t	size;
-
-	size = 0;
-	if (n <= 0)
-		size++;
-	while (n)
+	if (n < 0)
 	{
-		n /= 10;
-		size++;
+		num[0] = '-';
+		n *= -1;
 	}
-	return (size);
+	if (n >= 10)
+		ft_int_to_char((n / 10), num, (i -1));
+	num[i] = (n % 10) + '0';
 }
 
 char	*ft_itoa(int n)
 {
-	size_t	size;
-	long	t_num;
-	char	*str;
+	char		*num_char;
+	long int	len;
 
-	t_num = n;
-	t_num = ft_abs(t_num);
-	size = ft_sizeint(n);
-	str = malloc((size + 1) * sizeof(char *));
-	if (!str)
+	len = ft_sizeint(n);
+	num_char = malloc((len +1) * sizeof(char));
+	if (!num_char)
 		return (NULL);
-	str[size--] = '\0';
-	while (t_num > 0)
-	{
-		str[size--] = t_num % 10 + '0';
-		t_num /= 10;
-	}
-	if (size == 0 && str[1] == '\0')
-		str[size] = '0';
-	else if (size == 0 && str[1] != '\0')
-		str[size] = '-';
-	return (str);
+	num_char[len--] = '\0';
+	ft_int_to_char(n, num_char, len);
+	return (num_char);
 }
