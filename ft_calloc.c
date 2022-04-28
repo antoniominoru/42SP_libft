becba63cee5ddeb925240290bbde993fbf3562b9
aminoru- <aminoru-@student.42sp.org.br>
2022-04-28 20:23:24 +0200

    updade

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,19 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 15:47:01 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/27 16:14:51 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/04/28 20:22:56 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	int		total;
 
-	total = count * size;
-	if (total / count != size)
-		return (NULL);
-	ptr = malloc(total);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, (total));
+	ft_bzero(ptr, (nmemb * size));
 	return (ptr);
 }
