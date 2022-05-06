f9b17ca9cd2fe984d4f919494b6d1137b0f19662
aminoru- <aminoru-@student.42sp.org.br>
2022-05-07 01:06:49 +0200

    fix bug - ft_strchr - discoved in get_next_line

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 01:13:39 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/04/27 15:20:15 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/05/07 00:50:25 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	char	ch;
 
 	ch = (unsigned char)c;
+	if (!s) // add
+		return (0);  // add
 	while (*s != '\0' && ch != *s)
 		s++;
 	if (ch == *s)
